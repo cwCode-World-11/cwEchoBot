038c56293f975e68b95e2c35390c5ce71ecc855e
cwCode-World-11 <cw1162000@gmail.com>
2024-08-25 17:48:08 +0530

    telegram bot ah deploy panna try pandren

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // https://rizkyrajitha.hashnode.dev/send-messages-with-telegram-using-appwrite-functions
 
 const { Telegraf } = require("telegraf");
-const app = require("express")
+// const app = require("express")
 
 const bot = new Telegraf("7126722358:AAEwnN3Pr71y6TCHS1jXh7yWyqa9wIshRx4");
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
 const startMsg = "You start this bot cwEchoBot\nSend any text to this bot and it will reply to you.\ndon't think so much just use and enjoy.";
 bot.start((ctx) => ctx.reply(startMsg));
